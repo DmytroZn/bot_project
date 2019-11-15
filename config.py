860bdd8374454579b66ae9de017b1a4c6d2a9279
Dmytrozn <Dmytro.zn.gmail.com>
2019-11-15 21:56:34 +0200

    add

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = '1064443122:AAFkqWdtmxtZJ5rKoPPP7OhRt5IgWTP0uXw'
 host = '127.0.0.1'
 
-webhook_url = f'https://34.70.5.164/{TOKEN[::-1]}' # set webhook for this url
+webhook_url = 'https://34.70.5.164'  #f'https://34.70.5.164/{TOKEN[::-1]}' # set webhook for this url
 handle_url = f''  #/{TOKEN[::-1]}/
 print(TOKEN[::-1])
