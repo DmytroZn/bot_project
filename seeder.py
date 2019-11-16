752b1c2c68616d8c6a74a8d59d5633b09ac1bc04
Dmytrozn <Dmytro.zn.gmail.com>
2019-11-16 13:10:56 +0200

    add comments

--- seeder.py
+++ seeder.py
@@ -7,23 +7,23 @@ connect('web_shop_bot')
 
 ##############################################
 
-# for i in range(5):
-#     obj = models.Category(**{'title':f'root {i}',
-#                         'description':f'descr {i}'}).save()
+for i in range(5):
+    obj = models.Category(**{'title':f'root {i}',
+                        'description':f'descr {i}'}).save()
 
-#     obj.add_subcategory(
-#         models.Category(**{'title': f'sub {i}',
-#                             'description': f'descr {i}'})
-#     )
+    obj.add_subcategory(
+        models.Category(**{'title': f'sub {i}',
+                            'description': f'descr {i}'})
+    )
 ###########################################
 
-# objects = models.Category.objects(parent__ne=None)
+objects = models.Category.objects(parent__ne=None)
 
-# for i in objects:
-#     i.add_subcategory(
-#         models.Category(**{'title': f'sub-sub {i}',
-#                             'description': f'd {i}'})
-#     )
+for i in objects:
+    i.add_subcategory(
+        models.Category(**{'title': f'sub-sub {i}',
+                            'description': f'd {i}'})
+    )
 
 ###################################3
 
