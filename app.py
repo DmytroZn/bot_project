35163d3caf80a4bc6121c10b4060cd6c5976c422
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-02 14:17:17 +0200

    ad

--- app.py
+++ app.py
@@ -17,29 +17,18 @@ app = Flask(__name__)
 
 
 
-#
-from flask import Flask, request, Response
-# from models.workers import Person
-# from schemes.workers_schema import PersonSchema
-from flask_restful import Api
-from resources.product_resources import ProductResource
 
-api = Api(app)
-print('work1')
-api.add_resource(ProductResource, '/product', '/product/<string:id>')
-print('work2')
-#
 
 # Process webhook calls
-# @app.route('/', methods=['POST'])  #config.handle_url
-# def webhook():
-#     if request.headers.get('content-type') == 'application/json':
-#         json_string = request.get_data().decode('utf-8')
-#         update = telebot.types.Update.de_json(json_string)
-#         bot.process_new_updates([update])
-#         return ''
-#     else:
-#         abort(403)
+@app.route('/', methods=['POST'])  #config.handle_url
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
 
 
 @bot.message_handler(commands=['start'])
