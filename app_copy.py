e06d06ac46b2ecb5a6bc6f840488185fd5c422bc
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-03 22:48:08 +0200

    a

--- app_copy.py
+++ app_copy.py
@@ -36,8 +36,8 @@ import config
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(ProductResource, '/product', '/product/<string:id>')
-api.add_resource(CategoryResource, '/category', '/category/<string:id>')
+api.add_resource(ProductResource, '/product/', '/product/<string:id>')
+api.add_resource(CategoryResource, '/category/', '/category/<string:id>')
 
 
 
