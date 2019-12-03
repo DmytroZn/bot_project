0fb9302e8924bf5e4eaa5718476cfa526769e6b2
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-03 22:38:36 +0200

    a

--- app_copy.py
+++ app_copy.py
@@ -32,8 +32,8 @@ from flask import Flask, request, Response
 # from schemes.workers_schema import PersonSchema
 from flask_restful import Api
 from resources.product_resources import *  #ProductResource
-app_copy = Flask(__name__)
-api = Api(app_copy)
+app = Flask(__name__)
+api = Api(app)
 
 api.add_resource(ProductResource, '/product', '/product/<string:id>')
 api.add_resource(CategoryResource, '/category', '/category/<string:id>')
@@ -58,4 +58,5 @@ api.add_resource(CategoryResource, '/category', '/category/<string:id>')
 
 
 if __name__ == '__main__':
-    app_copy.run(port=5001, debug=True)
+    # app.run(port=5001, debug=True)
+    app.run(debug=True, port=config.HOST_PORT)
