c384cc5572fb003c1cd33c89118b5144dbf6edba
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-03 22:41:57 +0200

    a

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 TOKEN = '1064443122:AAH4bx3gaDhBoO636j0PTeV7CDZrCoYC3kQ'
+HOST_PORT = "4000"
 host = '127.0.0.1'
 
 webhook_url = 'https://34.70.5.164'  #f'https://34.70.5.164/{TOKEN[::-1]}' # set webhook for this url
