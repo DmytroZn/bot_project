9c48c0247fc18e1f455e3ae490329b21cfe4eac0
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-04 23:53:54 +0200

    changed

--- app_copy.py
+++ app_copy.py
@@ -1,43 +1,21 @@
-# from flask import Flask, request, Response
-# from models.workers import Person
-# from schemes.workers_schema import PersonSchema
-# from flask_restful import Api
-# from resources.worker_resourses import WorkerResource
-# app = Flask(__name__)
-# api = Api(app)
-
-# api.add_resource(WorkerResource, '/workers', '/<string:id>')
-# # app = Flask(__name__)
-
-
-# # @app.route('/', methods = ['GET', 'POST'])
-# # def hello_world():
-# #     if request.method == 'GET':
-# #         obj = Person.objects.get()
-# #         return PersonSchema().dump(obj)
-# #     else:
-# #         validity =PersonSchema().validate(request.json)
-# #         if validity:
-# #             return validity
-# #         Person(**request.json).save()
-# #         return Response(status=201)
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, Response
 # from models.workers import Person
 # from schemes.workers_schema import PersonSchema
 from flask_restful import Api
-from resources.product_resources import *  #ProductResource
-import config
+from resources.product_resources import * 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ProductResource, '/product/', '/product/<string:id>')
 api.add_resource(CategoryResource, '/category/', '/category/<string:id>')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
 
 
 
@@ -55,9 +33,3 @@ api.add_resource(CategoryResource, '/category/', '/category/<string:id>')
 #             return validity
 #         Person(**request.json).save()
 #         return Response(status=201)
-
-
-
-if __name__ == '__main__':
-    # app.run(port=5001, debug=True)
-    app.run(debug=True, port=config.HOST_PORT)
--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 TOKEN = '1064443122:AAH4bx3gaDhBoO636j0PTeV7CDZrCoYC3kQ'
-HOST_PORT = "5001"
 host = '127.0.0.1'
 
 webhook_url = 'https://34.70.5.164'  #f'https://34.70.5.164/{TOKEN[::-1]}' # set webhook for this url
