d82d4c514fd90c7ec803c3a726786e14533ee97b
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-07 17:52:18 +0200

    change token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '1064443122:AAH4bx3gaDhBoO636j0PTeV7CDZrCoYC3kQ'
+TOKEN = '1064443122:AAGhsAGr05K6FarnKSG11ogeePBShPQpbq8'
 host = '127.0.0.1'
 
 webhook_url = 'https://34.70.5.164'  #f'https://34.70.5.164/{TOKEN[::-1]}' # set webhook for this url
